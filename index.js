d1f7b608016c340fb6f3d5cc4c88649f7ed4028f
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-04-11 01:48:35 +0200

    Changed formatDate function

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const formatDate = (timeInSeconds) => {
-  return `${timeInSeconds}s`;
-}
+const formatDate = time => {
+  // return `${timeInSeconds}s`;
+
+  var hours = Math.floor(time / 3600);
+  var minutes = Math.floor((time % 3600) / 60);
+  var seconds = Math.floor(time % 60);
+
+  if (time < 60) {
+    return time + "s";
+  } else if (!time) {
+    return 0 + "s";
+  } else if (seconds === 0 && hours === 0) {
+    return minutes + "m";
+  } else if (hours === 0) {
+    return minutes + "m " + seconds + "s";
+  } else if (minutes === 0 && seconds === 0) {
+    return hours + "h";
+  } else if (seconds === 0) {
+    return hours + "h " + minutes + "m";
+  } else if (minutes === 0) {
+    return hours + "h " + seconds + "s";
+  } else {
+    return hours + "h " + minutes + "m " + seconds + "s";
+  }
+};
 
 module.exports = formatDate;
