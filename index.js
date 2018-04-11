ff4247025677aedad1af0e1e6121788ead05e960
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-04-11 23:29:44 +0200

    Function optimization

--- index.js
+++ index.js
@@ -5,23 +5,38 @@ const formatDate = time => {
   var minutes = Math.floor((time % 3600) / 60);
   var seconds = Math.floor(time % 60);
 
-  if (time < 60) {
-    return time + "s";
-  } else if (!time) {
-    return 0 + "s";
-  } else if (seconds === 0 && hours === 0) {
-    return minutes + "m";
-  } else if (hours === 0) {
-    return minutes + "m " + seconds + "s";
-  } else if (minutes === 0 && seconds === 0) {
-    return hours + "h";
-  } else if (seconds === 0) {
-    return hours + "h " + minutes + "m";
-  } else if (minutes === 0) {
-    return hours + "h " + seconds + "s";
-  } else {
-    return hours + "h " + minutes + "m " + seconds + "s";
+  // if (time < 60) {
+  //   return time + "s";
+  // } else if (!time) {
+  //   return 0 + "s";
+  // } else if (seconds === 0 && hours === 0) {
+  //   return minutes + "m";
+  // } else if (hours === 0) {
+  //   return minutes + "m " + seconds + "s";
+  // } else if (minutes === 0 && seconds === 0) {
+  //   return hours + "h";
+  // } else if (seconds === 0) {
+  //   return hours + "h " + minutes + "m";
+  // } else if (minutes === 0) {
+  //   return hours + "h " + seconds + "s";
+  // } else {
+  //   return hours + "h " + minutes + "m " + seconds + "s";
+  // }
+
+    if (!time) {
+    return "0s";
   }
+  var result = "";
+  if (hours) {
+    result += hours + "h ";
+  }
+  if (minutes) {
+    result += minutes + "m ";
+  }
+  if (seconds) {
+    result += seconds + "s ";
+  }
+  return result.slice(0, -1); // odcinamy ostatnią spację
 };
 
 module.exports = formatDate;
